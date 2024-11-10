34c84b1572294303204a4eb21d903daa68486e73
VolodymyrSinitsyn555 <vladiofun555@gmail.com>
2024-11-10 13:06:20 +0100

    commit new

---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Lesson06 from "./lessons/lesson06/Lesson06";
+import Lesson07 from "./lessons/lesson07/Lesson07";
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <>
+    {/* <Lesson06/> */}
+    <Lesson07/>
+  </>
+);
+
+
+
+
